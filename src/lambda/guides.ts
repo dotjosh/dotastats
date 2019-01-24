4690f53195f65da14b389e76c04f0f29ab03e180
Josh Schwartzberg <jschwartzberg@cobalttech.com>
2019-01-24 15:33:51 -0500

    Lambda functions now Typescript -- sharing types from client to server

--- src/lambda/guides.ts
+++ src/lambda/guides.ts
@@ -2,13 +2,14 @@
 
 import fetch from "node-fetch";
 import cheerio from "cheerio";
-import orderBy from "lodash.orderby";
+import { GuideResponse, Guide } from "../types"
+import { Handler, Context, APIGatewayEvent } from 'aws-lambda';
 
-exports.handler = async (event, context) => {
+export const handler: Handler = async (ev: APIGatewayEvent, context: Context) => {
 	const hero =
-		(event.queryStringParameters &&
-			event.queryStringParameters.hero &&
-			event.queryStringParameters.hero.toLowerCase().replace(" ", "-")) ||
+		(ev.queryStringParameters &&
+			ev.queryStringParameters.hero &&
+			ev.queryStringParameters.hero.toLowerCase().replace(" ", "-")) ||
 		"medusa";
 
 	const promises = [];
@@ -16,7 +17,7 @@ exports.handler = async (event, context) => {
 		promises.push(getPage(page, hero));
 	}
 
-	const finalResult = [];
+	const finalResult: Guide[] = [];
 	await Promise.all(promises).then(results => {
 		results.forEach(result => {
 			finalResult.push(...result);
@@ -27,18 +28,18 @@ exports.handler = async (event, context) => {
 		statusCode: 200,
 		body: JSON.stringify({
 			result: finalResult
-		}),
+		} as GuideResponse),
 		headers: {
 			"Access-Control-Allow-Origin": "*", // Required for CORS support to work
 			"Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
 		}
 	};
-};
+}
 
-const getPage = async (page, hero) => {
+const getPage = async (page: number, heroName: string): Promise<Guide[]> => {
 	const pageStr = page > 1 ? `?page=${page}` : "";
 	const response = await fetch(
-		`https://www.dotabuff.com/heroes/${hero.replace("'", "")}/guides${pageStr}`
+		`https://www.dotabuff.com/heroes/${heroName.replace("'", "")}/guides${pageStr}`
 	);
 	const html = await response.text();
 	const $ = cheerio.load(html);
--- src/lambda/heroes.ts
+++ src/lambda/heroes.ts
@@ -1,15 +1,17 @@
 "use strict";
 
-import fetch from "node-fetch";
+import fetch from "node-fetch"; 
 import cheerio from "cheerio";
+import { HeroResponse, Hero } from "../types"
+import { Handler, Context, APIGatewayEvent } from 'aws-lambda';
 
-exports.handler = async (event, context) => {
+export const handler: Handler = async (ev: APIGatewayEvent, context: Context) => {
 	const response = await fetch("https://www.dotabuff.com/heroes");
 	const html = await response.text();
 	const $ = cheerio.load(html);
 	const result = $("div.hero-grid .hero")
 		.get()
-		.map(x => ({
+		.map<Hero>(x => ({
 			name: $(x)
 				.find(".name")
 				.text(),
@@ -21,10 +23,11 @@ exports.handler = async (event, context) => {
 		statusCode: 200,
 		body: JSON.stringify({
 			result: result
-		}),
+		} as HeroResponse),
 		headers: {
 			"Access-Control-Allow-Origin": "*", // Required for CORS support to work
 			"Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
 		}
 	};
-};
+}; 
+
